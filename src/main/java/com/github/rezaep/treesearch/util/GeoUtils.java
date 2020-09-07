fac0b59c004a11958171848f77efd590ba02e1b4
Reza Ebrahimpour <r.ebrahimpour93@gmail.com>
2020-09-07 12:27:01 +0430

    add javadoc

--- src/main/java/com/github/rezaep/treesearch/util/GeoUtils.java
+++ src/main/java/com/github/rezaep/treesearch/util/GeoUtils.java
@@ -11,10 +11,24 @@ public class GeoUtils {
         return meter * 0.00001 / 0.941;
     }
 
+    /**
+     * Create point from the given latitude and longitude.
+     *
+     * @param latitude  the latitude of the point
+     * @param longitude the longitude of the point
+     * @return the point
+     */
     public static Point createPoint(double latitude, double longitude) {
         return new GeometryFactory().createPoint(new Coordinate(longitude, latitude));
     }
 
+    /**
+     * Create a circle with the the given center and meter
+     *
+     * @param center center of the circle
+     * @param meter  radius in meter
+     * @return circle
+     */
     public static Geometry createCircle(Point center, double meter) {
         double radius = GeoUtils.convertMeterToRadius(meter);
         GeometricShapeFactory shapeFactory = new GeometricShapeFactory();
